23482e727af768789f286b498dece1867b863cde
vovod <nguyenhaid0212@gmail.com>
2023-10-06 17:43:55 +0700

    1

---
+++ main.js
@@ -0,0 +1,17 @@
+//Bien
+    var fullName = 'vovod';
+//alert mention
+    alert(fullName);
+    console.log('F12 console');//console
+    confirm('Day');//Header
+    prompt('Day?')//Header key
+//Time after
+    setTimeout(function(){
+        alert('2 sec');
+    }, 2000);
+//Loop
+/*
+    setInterval(function() {
+        console.log('Random ' + (Math.floor(Math.random() * 100) + 1))
+    }, 5000);
+*/
